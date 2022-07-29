25cd5c68a48b67368c699ebb1b4b3ea6d78b08fd
tommyprand <tommy.prando@gmail.com>
2022-07-29 21:02:56 +0200

    Fixed crash when switching master client

--- dwm.c
+++ dwm.c
@@ -152,6 +152,7 @@ static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interac
 static void arrange(Monitor *m);
 static void arrangemon(Monitor *m);
 static void attach(Client *c);
+static void attachabove(Client *c);
 static void attachstack(Client *c);
 static void buttonpress(XEvent *e);
 static void checkotherwm(void);
@@ -422,9 +423,15 @@ arrangemon(Monitor *m)
 void
 attach(Client *c)
 {
+	c->next = c->mon->clients;
+	c->mon->clients = c;
+}
+
+void
+attachabove(Client *c)
+{
 	if (c->mon->sel == NULL || c->mon->sel == c->mon->clients || c->mon->sel->isfloating) {
-		c->next = c->mon->clients;
-		c->mon->clients = c;
+		attach(c);
 		return;
 	}
 	
@@ -1105,7 +1112,7 @@ manage(Window w, XWindowAttributes *wa)
 		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
 		XRaiseWindow(dpy, c->win);
-	attach(c);
+	attachabove(c);
 	attachstack(c);
 	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
 		(unsigned char *) &(c->win), 1);
@@ -1469,7 +1476,7 @@ sendmon(Client *c, Monitor *m)
 	detachstack(c);
 	c->mon = m;
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
-	attach(c);
+	attachabove(c);
 	attachstack(c);
 	focus(NULL);
 	arrange(NULL);
@@ -1979,7 +1986,7 @@ updategeom(void)
 				m->clients = c->next;
 				detachstack(c);
 				c->mon = mons;
-				attach(c);
+				attachabove(c);
 				attachstack(c);
 			}
 			if (m == selmon)
